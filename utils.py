cd11afe769062b96e6b270b31f2e6afbdb886b00
cwl-uml <38968787+cwl-uml@users.noreply.github.com>
2020-04-18 21:35:59 -0500

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,19 @@
+import os
+import requests
+
+
+def download(media_url, size, outdir):
+    r = requests.get(media_url + ':' + size, stream=True)
+    bs = os.path.basename(media_url)
+    filename = os.path.join(outdir or '', bs)
+    if '?' in filename:
+        filename = filename.split('?')[0]
+    with open(filename, 'wb') as fd:
+        for chunk in r.iter_content(chunk_size=1024):
+            fd.write(chunk)
+    return filename
+
+
+def create_directory(d):
+    if not os.path.exists(d):
+        os.makedirs(d)
